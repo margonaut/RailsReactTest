bcf72bfb8ee24aa534a89db68b5376e55ecf0455
margonaut <margaret.morgane@gmail.com>
2016-07-26 17:12:40 -0400

    Comment creation to DB now hooked up

--- app/assets/javascripts/comments.js.jsx
+++ app/assets/javascripts/comments.js.jsx
@@ -44,21 +44,88 @@ var CommentBox = React.createClass({
       }.bind(this)
     });
   },
+  handleCommentSubmit: function(comment) {
+    var comments = this.state.comments;
+    var newComments = comments.concat([comment]);
+    this.setState({comments: newComments});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: {'comment': comment},
+      success: function(data) {
+        this.loadCommentsFromServer();
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="CommentBox">
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
         <CommentList comments={this.state.comments} />
       </div>
     )
   }
 });
 
+var CommentForm = React.createClass({displayName: 'CommentForm',
+      getInitialState: function() {
+        return { author: '', comment: '' };
+      },
+      handleAuthorChange: function(e) {
+        // called when the corresponding input value is changed. The event
+        // containing the new data is passed in and used to update the value
+        // in the state
+        this.setState({author: e.target.value});
+      },
+      handleTextChange: function(e) {
+        this.setState({comment: e.target.value});
+      },
+      handleSubmit: function(e) {
+        // Prevent the page from refreshing
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var comment = this.state.comment.trim();
+        // If the author or comment value isn't present, return out of the method
+        // and do nothing
+        if (!comment || !author) {
+          return;
+        }
+        // activate function passed down from parent for this purpose
+        this.props.onCommentSubmit({author: author, comment: comment});
+
+
+        // And reset the form placeholder values.
+        // not working for some reason
+        // this.setState({author: '', comment: ''});
+      },
+      render: function() {
+        // When the value of this input is changed, call the method
+        // that sets that value in the component state
+        // onChange is a react event handler
+        return (
+          <form className="commentForm" onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder={this.state.author}
+              onChange={this.handleAuthorChange}
+            />
+            <input
+              type="text"
+              placeholder={this.state.comment}
+              onChange={this.handleTextChange}
+            />
+            <input type="submit" value="Post" />
+          </form>
+        )
+      }
+    });
+
 
 var ready = function() {
-  var fakeComments = [
-    { author:"Richard", comment:"This is a comment" },
-    { author:"Nils", comment:"This is another comment" }
-  ];
 
   ReactDOM.render(
     <CommentBox url="/comments.json" />,
